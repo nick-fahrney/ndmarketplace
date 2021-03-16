18c356f2856055ad83a13ba854e02f4c2f1b3fda
Conor Rinehart <cnrinehart@gmail.com>
2021-03-15 23:14:43 -0400

    Recent changes

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import {
     html,
-    render
+    render,
+    useEffect,
+    useState
 } from "https://unpkg.com/htm/preact/standalone.module.js";
 import { getSellers } from "./data.js";
 
 import Parent from "./Parent.js";
 
 function App() {
+
+  const [sellers, setSellers] = useState([]);
+
+  useEffect(() => {
+    return getSellers().then((data) => {
+      setSellers(data);
+    });
+  });
 
   //const [sellData, setSellData] = useState([]);
 
@@ -17,7 +27,7 @@ function App() {
   //});
 
 
-  return html` <${Parent} /> `;
+  return html` <${Parent} objs=sellers/> `;
 }
 
 render(html` <${App} /> `, document.getElementById("app"));
--- Child.js
+++ Child.js
@@ -18,7 +18,7 @@ export default function Child() {
     });
   });
 
-  console.log(sellers);
+// here we are having problems with using the values of the checkboxes to filter which products get shown. If you uncomment the indicated sections you can see that we did get a css grid working with data and images pulled from
 
   return html`
     <div class="nav">
@@ -35,33 +35,37 @@ export default function Child() {
         </p>
       </form>
     </div>
-    <div class="child">
 
-        ${sellers.map(
-          (seller) =>
-            html`<article key="${seller}" class="itemSale">
-              ${seller.title} | ${seller.image} ${seller.price}
-              </article>`
-            )}
-      <article> 1 </article>
-      <article> 2 </article>
-      <article> 3 </article>
-      <article> 4 </article>
-      <article> 5 </article>
-      <article> 6 </article>
-    </div>
+
 
     <div class="filter">
       <form action="http://www.example.com/subscribe.php">
         <p>Show items:
           <ul>
-            <li><input type="checkbox" name="choice" value="seating" checked="checked"/>Seating</li>
-            <li><input type="checkbox" name="choice" value="electronics" checked="checked"/>Electronics</li>
-            <li><input type="checkbox" name="choice" value="books" checked="checked"/>Books</li>
-            <li><input type="checkbox" name="choice" value="clothes" checked="checked"/>Clothes</li>
+            <li><input type="checkbox" name="choice" id="seating" checked="checked"/>Seating</li>
+            <li><input type="checkbox" name="choice" id="electronics" checked="checked"/>Electronics</li>
+            <li><input type="checkbox" name="choice" id="books" checked="checked"/>Books</li>
+            <li><input type="checkbox" name="choice" id="clothes" checked="checked"/>Clothes</li>
           </ul>
         </p>
       </form>
+    </div>
+
+
+
+    <div class="child">
+        ${sellers.map(
+          (seller) =>
+             // console.log(seller.itemType);
+            // console.log(document.getElementById(seller.itemType).checked);
+            // if (document.getElementById(seller.itemType).checked == true){
+              // console.log("in if");
+            html`<article key="${seller}" class="itemSale">
+              <img src="${seller.image}" width="150" height="150"/>  ${seller.title} ${seller.price}
+              </article>`
+            //}
+            )
+            }
     </div>
 
   `;
